c38871aecd0cba13a5bf4f23335628be0ec0be32
marcprux <marcprux@users.noreply.github.com>
2023-01-04 06:56:29 +0000

    deploy: e7572e4615f98d3368634dd0912397b8b88108ab

--- Package.swift
+++ Package.swift
@@ -9,16 +9,16 @@ let package = Package(
         .library(name: "PetStore", targets: ["PetStore"]),
     ],
 dependencies: [ .package(name: "swift-docc-plugin", url: "https://github.com/apple/swift-docc-plugin", from: "1.0.0"),
-        .package(url: "https://github.com/jectivex/JXPod", from: "0.2.0"),
+        .package(url: "https://github.com/jectivex/JXBridge.git", from: "0.1.14"),
         .package(url: "https://github.com/jectivex/JXSwiftUI", from: "0.1.7"),
     ],
     targets: [
         .target(name: "PetStore", dependencies: [
-            .product(name: "JXPod", package: "JXPod"),
+            .product(name: "JXBridge", package: "JXBridge"),
             .product(name: "JXSwiftUI", package: "JXSwiftUI"),
         ], resources: [
             .process("Resources"),
-            .copy("jxmodule"),
+            .copy("jsmodules"),
         ]),
         .testTarget(
             name: "PetStoreTests",
