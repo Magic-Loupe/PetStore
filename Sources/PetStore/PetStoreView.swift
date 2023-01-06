d866658f4dd3afc7fefe1ac64d6238dd4ddfb4be
Abe White <abewhite@mac.com>
2023-01-06 00:05:34 -0600

    Update for modal presentation

--- Sources/PetStore/PetStoreView.swift
+++ Sources/PetStore/PetStoreView.swift
@@ -4,6 +4,7 @@ import SwiftUI
 
 public struct PetStoreView: View {
     let context: JXContext
+    @Environment(\.dismiss) private var dismiss
     @StateObject private var model: PetStoreModel = {
         let model = PetStoreModel()
         model.load()
@@ -20,8 +21,17 @@ public struct PetStoreView: View {
     }
     
     public var body: some View {
-        JXView(context: context) { context in
-            return try context.new("petstore.PetListView", withArguments: [model])
+        NavigationView {
+            JXView(context: context) { context in
+                return try context.new("petstore.PetListView", withArguments: [model])
+            }
+            .toolbar {
+                ToolbarItem(placement: .navigationBarLeading) {
+                    Button(action: { dismiss() }) {
+                        Image(systemName: "xmark.circle.fill")
+                    }
+                }
+            }
         }
     }
 }
